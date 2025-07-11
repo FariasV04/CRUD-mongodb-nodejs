7757a9e1f010217b79c39bffadb35515ab985458
Farias <felipefarias20014@gmail.com>
2025-07-11 14:27:53 -0300

    Conexão com o MongoDB

---
+++ server.js
@@ -0,0 +1,23 @@
+// das Bibliotecas essenciais 
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+//configuração da variavel de ambiente 
+dotenv.config();
+
+//configuração do Servidor
+const app = express();
+const port = 3000;
+
+//conexão com Banco de Dados
+const ConectDB = async() => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Conectado ao MongoDB");
+    } catch (error) {
+        console.log("Deu Erro ao conectar com o MongoDB", error);
+    }
+}
+
+app.listen(port, () => console.log(`O Servidor está rodando a porta ${port}`))
